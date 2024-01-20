63f5a0f4de40f1a66206ffa16ac01a7974dc2e36
AccountMonitor <155661825+AccountMonitor@users.noreply.github.com>
2024-01-20 13:59:45 +0300

    sync

--- users/models.py
+++ users/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     phone = models.CharField(max_length=250)
-    email = models.CharField(max_length=250)
     password = models.CharField(max_length=250)
     business_name = models.CharField(max_length=250, blank=True, null=True)
     username = models.CharField(max_length=250, unique=True)
