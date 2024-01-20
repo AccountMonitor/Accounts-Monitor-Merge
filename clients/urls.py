5b158d847cf3546ad81c1fc8bb6e53a220b33a20
AccountMonitor <155661825+AccountMonitor@users.noreply.github.com>
2024-01-20 13:58:05 +0300

    sync

--- clients/urls.py
+++ clients/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 from refferals.forms import LoginForm
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('registeration', views.registration, name='registration'),
@@ -9,4 +10,10 @@ urlpatterns = [
     path('customer_invoice/<int:id>/', views.editInvoice, name='edit_invoice'),
     path('customer_invoice/', views.customerInvoice, name='customer_invoice'),
     path('login/', auth_views.LoginView.as_view(template_name = 'login.html', authentication_form = LoginForm), name ='login'),
+    
+    #passoword reset urls    
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
